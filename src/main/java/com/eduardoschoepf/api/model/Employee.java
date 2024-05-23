be316eb733493d732b21bcd43e0bca0b2de04c9a
eschoepf <eduardo.schoepf@hotmail.com>
2024-05-23 22:36:22 +0200

    Adjust indents

--- src/main/java/com/eduardoschoepf/api/model/Employee.java
+++ src/main/java/com/eduardoschoepf/api/model/Employee.java
@@ -9,23 +9,23 @@ import javax.persistence.Table;
 
 import lombok.Data;
 
-@Data                           // Est une annotation Lombok, la librairie s’en charge d’ajouter les getters et les setters 
-@Entity                         // Est une annotation qui indique que la classe correspond à une table de la base de données
-@Table(name = "employees")      // Indique le nom de la table associée
+@Data                                                   // Est une annotation Lombok, la librairie s’en charge d’ajouter les getters et les setters 
+@Entity                                                 // Est une annotation qui indique que la classe correspond à une table de la base de données
+@Table(name = "employees")                              // Indique le nom de la table associée
 public class Employee {
 
-    @Id                         // L’attribut id correspond à la clé primaire de la table, et est donc annoté @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // comme l’id est auto-incrémenté, j’ai ajouté l’annotation @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id                                                 // L’attribut id correspond à la clé primaire de la table, et est donc annoté @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Comme l’id est auto-incrémenté, j’ai ajouté l’annotation @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="first_name")  // L'annotation @Column pour faire le lien avec le nom du champ de la table
+    @Column(name="first_name")                          // L'annotation @Column pour faire le lien avec le nom du champ de la table
     private String firstName;
 
-    @Column(name="last_name")   // L'annotation @Column pour faire le lien avec le nom du champ de la table
+    @Column(name="last_name")                           // L'annotation @Column pour faire le lien avec le nom du champ de la table
     private String lastName;
 
-    private String mail;       // Pas besoin de mettre l'annotation @Column pour mail car le nom du champ de la table est identique
+    private String mail;                                // Pas besoin de mettre l'annotation @Column pour mail car le nom du champ de la table est identique
 
-    private String password;   // Pas besoin de mettre l'annotation @Column pour password car le nom du champ de la table est identique
+    private String password;                            // Pas besoin de mettre l'annotation @Column pour password car le nom du champ de la table est identique
 
 }
