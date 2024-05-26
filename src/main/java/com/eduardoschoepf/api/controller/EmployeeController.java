10b9b1d1e2f5f4eb79edb93cababf7f91ea4168b
eschoepf <eduardo.schoepf@hotmail.com>
2024-05-26 16:08:36 +0200

    REFACTORING

--- src/main/java/com/eduardoschoepf/api/controller/EmployeeController.java
+++ src/main/java/com/eduardoschoepf/api/controller/EmployeeController.java
@@ -7,16 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.eduardoschoepf.api.model.Employee;
 import com.eduardoschoepf.api.service.EmployeeService;
 
-@RestController // Indique à Spring d’insérer le retour de la méthode au format JSON dans le corps de la réponse HTTP
+//Indique à Spring d’insérer le retour de la méthode au format JSON dans le corps de la réponse HTTP
+@RestController 
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    /**
-    * Read - Get all employees
-    * @return - An Iterable object of Employee full filled
-    */
     @GetMapping("/employees")
     public Iterable<Employee> getEmployees() {
         return employeeService.getEmployees();
--- src/main/java/com/eduardoschoepf/api/service/EmployeeService.java
+++ src/main/java/com/eduardoschoepf/api/service/EmployeeService.java
@@ -1,19 +1,21 @@
 package com.eduardoschoepf.api.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import lombok.Data;
+
 import com.eduardoschoepf.api.model.Employee;
 import com.eduardoschoepf.api.repository.EmployeeRepository;
-
-import lombok.Data;
 
 @Data
 @Service
 public class EmployeeService {
 
     @Autowired
+    @Qualifier("employeeRepository")
     private EmployeeRepository employeeRepository;
 
     public Optional<Employee> getEmployee(final Long id) {
