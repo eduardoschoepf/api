ef21fff7ab02e47d4d5516ba229de7a708d97c43
eschoepf <eduardo.schoepf@hotmail.com>
2024-06-06 21:34:57 +0200

    Bug associated with the migration from javax to jakarta

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS employees;
+ 
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  first_name VARCHAR(250) NOT NULL,
+  last_name VARCHAR(250) NOT NULL,
+  mail VARCHAR(250) NOT NULL,
+  password VARCHAR(250) NOT NULL
+);
